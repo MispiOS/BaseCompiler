7675706c7a7078d21d08a260c31e1b9abb47c405
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-12-16 22:24:22 +0100

    init main

---
+++ src/fr/mispios/basecompiler/Main.java
@@ -0,0 +1,13 @@
+package fr.mispios.basecompiler;
+
+import fr.mispios.basecompiler.console.Console;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length == 1) { // the only needed argument is the base file
+            Console.printError("Invalid arguments number ! Got " + args.length + " expected 1.");
+            return;
+        }
+        // TODO
+    }
+}
