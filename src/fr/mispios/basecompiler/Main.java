43ee7ea417398ec6b0d8e11dafcd4e1b13245088
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-12-23 23:19:21 +0100

    Accept multiple files

--- src/fr/mispios/basecompiler/Main.java
+++ src/fr/mispios/basecompiler/Main.java
@@ -4,8 +4,8 @@ import fr.mispios.basecompiler.console.Console;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length == 1) { // the only needed argument is the base file
-            Console.printError("Invalid arguments number ! Got " + args.length + " expected 1.");
+        if(args.length < 1) { // the only needed argument is the base file
+            Console.printError("Invalid arguments number ! Got 0, minimum 1.");
             return;
         }
         // TODO
