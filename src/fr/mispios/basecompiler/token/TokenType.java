1e9404fe0120c93a7c5274f06e138f27d714f6e1
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-12-25 21:17:41 +0100

    Adding Token types and type from words

--- src/fr/mispios/basecompiler/token/TokenType.java
+++ src/fr/mispios/basecompiler/token/TokenType.java
@@ -1,6 +1,7 @@
 package fr.mispios.basecompiler.token;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,14 +9,21 @@ import java.util.List;
  */
 @SuppressWarnings("unused")
 public enum TokenType {
-    NONE,
+    OTHER,
+    KEYWORD,
+    TYPE,
     EQUALS('='),
     QUOTE('"'),
     CHAR_QUOTE('\''),
     OPERATOR('+', '-', '/', '*', '%'),
+    COMPARATOR('<', '>', '&', '|', '^', '!'),
     BRACKET('[', ']'),
     BRACE('{', '}'),
-    PARENTHESIS('(', ')');
+    PARENTHESIS('(', ')'),
+    NUMBER('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'),
+    DOT('.'),
+    SEMICOLON(';'),
+    POINTER('$');
 
     private final List<Character> symbol;
     TokenType(char... symbols) {
@@ -27,22 +35,43 @@ public enum TokenType {
 
     public List<Character> getSymbols() { return symbol; }
 
+    private static final List<String> keywords = Arrays.asList(
+            "import", "if", "else", "while", "do", "break", "continue", "fnct", "return", "object", "init", "self"
+    );
+    private static final List<String> types = Arrays.asList("int", "float", "char", "void");
+
     /**
-     * Give the TokenType associated with the given symbol
+     * Give the <code>TokenType</code> associated with the given symbol
      * @param symbol The symbol you search the associated <code>TokenType</code>
-     * @return The associeated <code>TokenType</code> if it exists else <code>TokenType.NONE</code>
+     * @return The associated <code>TokenType</code> if it exists, <code>TokenType.OTHER</code> else
      */
     public static TokenType fromSymbol(char symbol) {
         TokenType[] values = values();
         int i = 0;
-        TokenType res = NONE;
+        TokenType res = OTHER;
         TokenType tmp;
-        while(i < values.length && res.equals(NONE)) {
+        while(i < values.length && res.equals(OTHER)) {
             tmp = values[i];
             if(tmp.getSymbols().contains(symbol)) {
                 res = tmp;
             }
             i++;
+        }
+        return res;
+    }
+
+    /**
+     * Give the <code>TokenType</code> associated with the given word
+     * @param word The word you search the associated <code>TokenType</code>
+     * @return The associated <code>TokenType</code> if it exists, <code>TokenType.OTHER</code> else
+     */
+    public static TokenType fromWord(String word) {
+        TokenType res = OTHER;
+
+        if(keywords.contains(word.toLowerCase())) {
+            res = KEYWORD;
+        } else if (types.contains(word.toLowerCase())) {
+            res = TYPE;
         }
         return res;
     }
