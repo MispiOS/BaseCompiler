fffbc1c7d593c622960b7581acebfba3756fa6d5
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2025-12-25 21:25:35 +0100

    JavaDoc

--- src/fr/mispios/basecompiler/token/IToken.java
+++ src/fr/mispios/basecompiler/token/IToken.java
@@ -1,9 +1,28 @@
 package fr.mispios.basecompiler.token;
 
 public interface IToken {
+    /**
+     * @return The file the Token has been generated from
+     */
     String getFile();
+
+    /**
+     * @return The line of the file the Token has been generated from
+     */
     int getLine();
+
+    /**
+     * @return The column of on the line the Token has been generated from
+     */
     int getColumn();
+
+    /**
+     * @return The <code>TokenType</code> of the Token
+     */
     TokenType getType();
+
+    /**
+     * @return The value of the Token
+     */
     String getValue();
 }
--- src/fr/mispios/basecompiler/token/ITokenizer.java
+++ src/fr/mispios/basecompiler/token/ITokenizer.java
@@ -4,5 +4,11 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface ITokenizer {
-    List<IToken> tokenize(String fileName) throws FileNotFoundException;
+    /**
+     * Transfrom the content of a file to a Token's list
+     * @param fileName The name of the file you want to transform into a Token's list
+     * @return A List of Tokens
+     * @throws FileNotFoundException
+     */
+    static List<IToken> tokenize(String fileName) throws FileNotFoundException {return null;};
 }
